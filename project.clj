6a33a616dfa0fd17b0b9de71b52978554004cb52
Ben Lovell <ben.j.lovell@gmail.com>
2018-03-27 15:53:20 +0200

    Added the brand new URL for the project!

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject geeny/fetch "0.1.0"
   :description "Seamlessly fetch data from an API in the middle of your Ring request"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/geeny/fetch"
   :license {:name "Mozilla Public License Version 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
   :dependencies [[org.clojure/clojure "1.8.0"]
